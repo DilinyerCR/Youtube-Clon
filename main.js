209dc18b92b0b1ca109f854cdac94319087cc14b
DilinyerCR <dilinyerecr@gmail.com>
2023-01-07 16:30:43 -0400

    primer commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('#container');
+const hide = document.querySelector('#hide');
+
+document.querySelector('#menu-button').addEventListener('click', () => {
+    container.classList.toggle('active');
+});
+
+// function myFunction() {
+//     if (container.classList !== "active") {
+//       hide.style.visibility = "hidden";
+//     } else {
+//       hide.style.visibility = "visible";
+//     }
+//   }
+
+// 
+
+
+
